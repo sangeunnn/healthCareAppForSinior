8b9a6ffebdf387d9363c90566c8098edb7f953cd
goo-gy <lkukh17@naver.com>
2020-08-20 23:15:25 +0900

    add excercise video
    add excercise video

--- senior_balance(android)/app/src/main/java/org/tensorflow/lite/examples/classification/VideoActivity.java
+++ senior_balance(android)/app/src/main/java/org/tensorflow/lite/examples/classification/VideoActivity.java
@@ -11,16 +11,21 @@ import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.source.ProgressiveMediaSource;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.ui.PlayerControlView;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.RawResourceDataSource;
 
 
 public class VideoActivity extends AppCompatActivity {
     PlayerView playerView;
+    PlayerControlView playerControlView;
+
     SimpleExoPlayer exoPlayer;
     String url;
     String str_model;
+    int rawVedieo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +33,9 @@ public class VideoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_video);
 
         playerView = findViewById(R.id.exo_view);
-        url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        playerControlView = findViewById(R.id.exo_control_view);
+        //url = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        rawVedieo = R.raw.stand_video;
         str_model = getIntent().getExtras().getString("model");
     }
 
@@ -37,9 +44,11 @@ public class VideoActivity extends AppCompatActivity {
         super.onStart();
         exoPlayer = ExoPlayerFactory.newSimpleInstance(this, new DefaultTrackSelector());
         playerView.setPlayer(exoPlayer);
+        playerControlView.setPlayer(exoPlayer);
 
         DataSource.Factory dataFactory = new DefaultDataSourceFactory(this, "Tester");
-        ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(dataFactory).createMediaSource(Uri.parse(url));
+        //ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(dataFactory).createMediaSource(Uri.parse(url));
+        ProgressiveMediaSource mediaSource = new ProgressiveMediaSource.Factory(dataFactory).createMediaSource(RawResourceDataSource.buildRawResourceUri(rawVedieo));
 
         exoPlayer.prepare(mediaSource);
         exoPlayer.setPlayWhenReady(true);
